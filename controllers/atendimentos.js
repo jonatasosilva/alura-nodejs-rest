b30436fccf0c872df8ada5f2b6ad887303b3087b
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2021-03-11 22:12:13 -0300

    streaming de dados e repositorio

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,11 @@
 const Atendimento = require("../models/atendimentos");
 
 module.exports = (app) => {
-  app.get("/atendimentos", (req, res) => Atendimento.lista(res));
+  app.get("/atendimentos", (req, res) => {
+    Atendimento.lista()
+      .then((resultados) => res.json(resultados))
+      .catch((erros) => res.status(400).json(erros));
+  });
 
   app.get("/atendimentos/:id", (req, res) => {
     const id = parseInt(req.params.id);
@@ -12,7 +16,11 @@ module.exports = (app) => {
   app.post("/atendimentos", (req, res) => {
     const atendimento = req.body;
 
-    Atendimento.adiciona(atendimento, res);
+    Atendimento.adiciona(atendimento)
+      .then((atendimentoCadastrado) =>
+        res.status(201).json(atendimentoCadastrado)
+      )
+      .catch((erros) => res.status(400).json(erros));
   });
 
   app.patch("/atendimentos/:id", (req, res) => {
---
+++ infraestrutura/database/tabelas.js
@@ -0,0 +1,36 @@
+class Tabelas {
+  init(conexao) {
+    this.conexao = conexao;
+
+    this.criarAtendimentos();
+    this.criarPets();
+  }
+
+  criarAtendimentos() {
+    const sql =
+      "CREATE TABLE IF NOT EXISTS atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))";
+
+    this.conexao.query(sql, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela atendimentos criada com sucesso");
+      }
+    });
+  }
+
+  criarPets() {
+    const query =
+      "CREATE TABLE IF NOT EXISTS pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY(id))";
+
+    this.conexao.query(query, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela pets foi criada com sucesso");
+      }
+    });
+  }
+}
+
+module.exports = new Tabelas();
